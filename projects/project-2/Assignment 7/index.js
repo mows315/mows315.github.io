36f9aa58485fd24e1ef91f760fa46e76e6a7a645
mows315 <mows315@newschool.edu>
2022-04-20 22:53:44 -0400

    apr 20
    apr 20

--- projects/project-2/Assignment 7/index.js
+++ projects/project-2/Assignment 7/index.js
@@ -3,18 +3,31 @@
 const airtableApiKey = "key0zgkz38dpB9LQp";
 
 // URL of Where our Data is Located
-const tableUrl = "https://api.airtable.com/v0/apphYBbLI307cYmgm/Table%201";
+const tableUrl = "https://api.airtable.com/v0/apphYBbLI307cYmgm/Table%202";
 
 // URL with API Key Authentication
 const authenticatedUrl = tableUrl + "?api_key=" + airtableApiKey;
 
+// Colors 
+const foregroundColors = ["#604F4F", "#895141", "#353A4C"] // 
+const backgroundColors = ["#EADFC9", "#80825B"]
+
 // SECTION 2: DOM References 
 const bodyElement = document.querySelector('body')
 const applicationElement = document.querySelector('#application')
-const fortitudeElement = document.querySelector('#fortitude')
+const adjectiveElement = document.querySelector('#adjective')
 const typeElement = document.querySelector('#type')
-const gardenElement = document.querySelector('#garden')
-const spyElement = document.querySelector('#spy')
+const verbElement = document.querySelector('#verb')
+const adverbElement = document.querySelector('#adverb')
+
+// SECTION 3: Application
+// Choose Foreground and Background Colors
+const foregroundColor = foregroundColors[Math.floor(Math.random()*foregroundColors.length)]
+const backgroundColor = backgroundColors[Math.floor(Math.random()*backgroundColors.length)]
+
+// Set Foreground and Background Colors 
+bodyElement.style.color = foregroundColor
+bodyElement.style.backgroundColor = backgroundColor
 
 // Get a 'Promise' Representing the Data that Will be Returned
 fetch(authenticatedUrl)
@@ -23,26 +36,24 @@ fetch(authenticatedUrl)
   // Use the JSON Data
   .then((data) => {
     const words = data.records;
-    console.log(words)
-    const fortitudes = words.filter((word) => {
-        return word.fields.Type === 'fortitude'
+    const adjectives = words.filter((word) => {
+        return word.fields.Type === 'adjective'
     })
     // Filter for Word Types 
-    const gardens = words.filter((word) => {
-        return word.fields.Type === 'garden'
-    })
     const types = words.filter((word) => {
         return word.fields.Type === 'type'
     })    
-    const spys = words.filter((word) => {
-        return word.fields.Type === 'spy'
+    const verbs = words.filter((word) => {
+        return word.fields.Type === 'verb'
+    })
+    const adverbs = words.filter((word) => {
+        return word.fields.Type === 'adverb'
     })
     // Set Values onto HTML Elements 
-    fortitudeElement.innerHTML = fortitudes[Math.floor(Math.random()*fortitudes.length)].fields.Copy
-    console.log(types)
+    adjectiveElement.innerHTML = adjectives[Math.floor(Math.random()*adjectives.length)].fields.Copy
     typeElement.innerHTML = types[Math.floor(Math.random()*types.length)].fields.Copy
-    gardenElement.innerHTML = gardens[Math.floor(Math.random()*gardens.length)].fields.Copy
-    spyElement.innerHTML = spys[Math.floor(Math.random()*spys.length)].fields.Copy
+    verbElement.innerHTML = verbs[Math.floor(Math.random()*verbs.length)].fields.Copy
+    adverbElement.innerHTML = adverbs[Math.floor(Math.random()*adverbs.length)].fields.Copy
     applicationElement.classList.add('loaded')
   })
   .catch((error) => {

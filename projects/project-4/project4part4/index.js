935c5a5865e605be9239e6ed30ce05c4429213b8
mows315 <mows315@newschool.edu>
2022-05-04 23:23:32 -0400

    may 4
    may 4

---
+++ projects/project-4/project4part4/index.js
@@ -0,0 +1,12 @@
+function showdiv() {
+    document.getElementById("resultat").style.display = "block";
+  }
+
+  document.querySelectorAll("img").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const image = event.target.getAttribute("data-src");
+      event.target.setAttribute("src", image);
+    });
+  });
+  
+  
